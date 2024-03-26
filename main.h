aca4d920a1d8336795747d5610973a70b424627f
Bakhish85 <umudlu85@gmail.com>
2024-03-26 09:31:08 +0000

    main.h

--- main.h
+++ main.h
@@ -1,11 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- * File: main.h
- *
- * Desc: Header file containing declarations for all functions
- *
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
  */
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list);
+} specifiers_t;
+/*prototypes*/
 int _printf(const char *format, ...);
 
 #endif
