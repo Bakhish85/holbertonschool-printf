6534a4ef7b28602ce273742c553e0f11848cafde
Bakhish85 <umudlu85@gmail.com>
2024-03-26 09:54:28 +0000

    main.h

--- main.h
+++ main.h
@@ -4,7 +4,32 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-int _printf(const char *format, ...);
+/**
+ * _printf - function that prints based on format specifier
+ * @format: takes in format specifier
+ * Return: return pointer index
+ */
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	int count;
+	format_t get_opt[] = {
+		{"c", set_char},
+		{"s", set_string},
+		{"i", set_decimal},
+		{"d", set_decimal},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	if (!format)
+	{
+		return (-1);
+	}
+	va_start(valist, format);
+	count = parse_format(format, get_opt, valist);
+	va_end(valist);
+	return (count);
+}
 
 /**
  * struct format - This is a typedef struct
