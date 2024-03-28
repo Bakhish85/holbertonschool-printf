1df62521a566bd24d2b49d34e378a65468fa803a
root <root@reone-pc>
2024-03-28 16:15:19 +0400

    upd rvn

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+/**
+  * _strlen - calculate string length
+  * @str: string
+  *
+  * Return: length of string
+  */
+int _strlen(char *str)
+{
+	char *s;
+
+	for (s = str; *s; s++)
+	{
+	};
+	return (s - str);
+}
