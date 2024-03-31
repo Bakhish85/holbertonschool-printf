3d7aaf4a0eb8500d29625abd20d90fdab2374a08
root <root@reone-pc>
2024-03-31 23:50:00 +0400

    upd rvn

--- main.h
+++ main.h
@@ -1,37 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
-#include <limits.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
-/**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: the function associated
- */
-
-typedef struct specifiers
-{
-	char specifiers;
-	int (*f)(va_list);
-} specifiers_t;
-
-/*prototypes*/
+#ifndef H_H
+#define H_H
 
 int _printf(const char *format, ...);
-int get_function(char s, va_list args);
-int _putchar(char c);
-void free_array(char *arr);
-int numlen(int num);
-int print_str(char *str);
-char *num_to_string(int num, char *str);
+void _putchar(char c);
 int _strlen(char *s);
 char *itoa(int a);
-/*Conversion specifiers*/
-int print_char(va_list args);
-
 #endif
